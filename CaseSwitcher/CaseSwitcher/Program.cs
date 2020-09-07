32d9a7d75e4b9b136c6c85f2d80aa1ff3c5417c7
Jakov <klaric9jakov@gmail.com>
2020-09-07 21:04:16 +0200

    Finished main function of program.

---
+++ CaseSwitcher/CaseSwitcher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CaseSwitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please enter a word or sentence.");
+            var userInput = Console.ReadLine();
+
+            Console.WriteLine(CaseSwitch(userInput));
+        }
+
+        static string CaseSwitch(string userInput)
+        {
+            var switchedUserInput = new StringBuilder();
+            var i = 0;
+
+            foreach (var character in userInput)
+            {
+                if (char.IsLower(character))
+                {
+                    switchedUserInput.Append(char.ToUpper(character));
+                }
+                else
+                {
+                    switchedUserInput.Append(char.ToLower(character));
+                }
+
+                i++;
+            }
+
+            return switchedUserInput.ToString();
+        }
+    }
+}
